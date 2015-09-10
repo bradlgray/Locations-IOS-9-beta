1678e7f638f6891ec0623003506bacc6c08563c3
Bradford Gray <bgray@westmont.edu>
2015-09-09 20:32:52 -0700

    UI created

---
+++ Locations IOS 9 beta/CurrentLocationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Locations IOS 9 beta
+//
+//  Created by Brad Gray on 9/9/15.
+//  Copyright © 2015 Brad Gray. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
